0c5f55efb66d1074f4c22156ea2ebc07a9f9e627
Dariel Buret <darielburetrivera@gmail.com>
2025-12-30 12:57:02 -0400

    Update digittools.py

--- digittools.py
+++ digittools.py
@@ -23,7 +23,42 @@ def subdigit_repeats(
     dtype: type | pl.DataType = pl.UInt8,
     keys_type: type | pl.DataType = pl.UInt64,
 ) -> pl.DataFrame:
-    """Return a DataFrame with subdigit repeats for a given number of digits."""
+    """Return a DataFrame with subdigit repeats for a given number of digits.
+
+    Parameters:
+        ndigits (int): The number of digits to generate subdigit repeats for.
+        gen_keys (bool): Whether to generate keys for each subdigit repeat.
+        dtype (type | pl.DataType): The data type to use for the subdigit and repeat columns.
+        keys_type (type | pl.DataType): The data type to use for the key column.
+
+        the keys represents the lowest possible subdigit repeat,
+        which is the smallest number that can be repeated to form the given number of digits.
+        This column is by default casted to UInt64.
+    Returns:
+        pl.DataFrame: A DataFrame with subdigit repeats for the given number of digits.
+
+    Example:
+        >>> subdigit_repeats(4)
+        ┌────────────┬─────────┐
+        │ subndigits ┆ repeats │
+        │ ---        ┆ ---     │
+        │ u8         ┆ u8      │
+        ╞════════════╪═════════╡
+        │ 1          ┆ 4       │
+        │ 2          ┆ 2       │
+        └────────────┴─────────┘
+
+        >>>subdigit_repeats(6, gen_keys=True, keys_type=str)
+        ┌────────────┬─────────┬────────┐
+        │ subndigits ┆ repeats ┆ key    │
+        │ ---        ┆ ---     ┆ ---    │
+        │ u8         ┆ u8      ┆ str    │
+        ╞════════════╪═════════╪════════╡
+        │ 1          ┆ 6       ┆ 111111 │
+        │ 2          ┆ 3       ┆ 010101 │
+        │ 3          ┆ 2       ┆ 001001 │
+        └────────────┴─────────┴────────┘
+    """
     initial_df = pl.DataFrame(
         ((1, ndigits),),
         dict.fromkeys(("subndigits", "repeats"), dtype),
@@ -75,5 +110,5 @@ def ndigits(n: int) -> int:
 
 
 if __name__ == "__main__":
-    print(subdigit_repeats(12))
+    print(subdigit_repeats(6, gen_keys=True, keys_type=str))
     print(ndigits(1234567890))
