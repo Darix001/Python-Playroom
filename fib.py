cf49237812881bb04542a0a6692907d1ade76853
Dariel Buret <darielburetrivera@gmail.com>
2026-01-07 08:43:44 -0400

    fib tests passed

--- fib.py
+++ fib.py
@@ -1,10 +1,10 @@
 import itertools as it
+import operator as op
 from collections import deque
 from collections.abc import Iterator
 from decimal import Decimal
 from functools import partial
 from math import trunc
-from operator import attrgetter
 
 fib_deque = partial(deque, (0, 1), maxlen=2)
 
@@ -22,7 +22,7 @@ def fib_gen():
 class Fib:
     __slots__ = "indexes"
     indexes: range
-    size = property(attrgetter("indexes.stop"))
+    size = property(op.attrgetter("indexes.stop"))
 
     def __init__(self, n: int, /):
         self.indexes = range(n)
@@ -49,33 +49,20 @@ class Fib:
         return f"Fib({self.size!r})"
 
     def __reversed__(self, /):  # PENDING
-        data = deque((-self[-1], self[-2]))
+        data = deque((self[-1], self[-2]), maxlen=2)
         if (n := self.size) > 2:
             n -= 2
         else:
             return reversed(data) if n == 2 else it.islice(data, n)
-        return map(
-            abs,
-            it.chain(
-                data,
-                it.filterfalse(data.append, map(sum, it.repeat(data, n))),
+        return it.chain(
+            data,
+            it.filterfalse(
+                data.append,
+                it.starmap(op.sub, map(op.itemgetter(0, -1), it.repeat(data, n))),
             ),
         )
 
     def to_list(self, /) -> list[int]:
-        seq = []
-        seq.extend(it.islice(it.accumulate(it.chain((0, 1), seq)), self.size))
+        seq = [0]
+        seq.extend(it.islice(it.accumulate(seq, initial=1), self.size - 1))
         return seq
-
-
-def main():
-    from sys import argv
-
-    n = int(argv[-1])
-
-    fb = Fib(n)
-    print(fb, fb.to_list() == list(fb))
-
-
-if __name__ == "__main__":
-    main()
