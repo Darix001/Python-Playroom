86ba076256842c1aff7fd6708a50d7ccd74dde08
Dariel Buret <darielburetrivera@gmail.com>
2026-01-06 21:03:35 -0400

    fib.py

--- fib.py
+++ fib.py
@@ -1,61 +1,82 @@
 import itertools as it
 from collections import deque
-from collections.abc import Iterable, Iterator
+from collections.abc import Iterator
+from decimal import Decimal
 from functools import partial
-from math import sqrt
+from math import trunc
+from operator import attrgetter
 
 import polars as pl
 
 fib_deque = partial(deque, (0, 1), maxlen=2)
-run_expr = pl.LazyFrame().select
+
+SQRT5 = Decimal(5).sqrt()
+PHI = (1 + SQRT5) / 2
+PSI = (1 - SQRT5) / 2
 
 
-def fib_func(func, /):
-    def function(n: int | None = None, /) -> Iterator[int]:
-        iterable = func(data := fib_deque(), n)
-        return it.chain(data, it.filterfalse(data.append, iterable))
-
-    return function
-
-
-@fib_func
-def fib(*args):
-    if args[-1] is None:
-        args = args[:-1]
-    return map(sum, it.repeat(*args))
-
-
-@fib_func
-def fib_until(data: deque[int], stop: int):
-    return iter(partial(sum, data), stop)
-
-
-five_sqrt = sqrt(5)
-phi = (1 + five_sqrt) / 2
-psi = (1 - five_sqrt) / 2
-fibc = pl.col.fib
-fib_expr = (phi**fibc - psi**fibc) / five_sqrt
-
-
-def polars_fib(
-    size: int | Iterable[int], dtype: pl.DataType | type | pl.DataTypeExpr
-) -> pl.LazyFrame:
-    if isinstance(size, int):
-        lf = pl.select(fib=pl.int_range(0, size, dtype=dtype), eager=False)
-    else:
-        lf = pl.LazyFrame({"fib": size}, {"fib": dtype})
-
-    return lf.select(
-        # Apply Binet's Formula
-        fib=fib_expr.cast(dtype)
+def fib_gen():
+    return it.chain(
+        data := fib_deque(), it.filterfalse(data.append, map(sum, it.repeat(data)))
     )
+
+
+class Fib:
+    __slots__ = "indexes"
+    indexes: range
+    size = property(attrgetter("indexes.stop"))
+
+    def __init__(self, n: int, /):
+        self.indexes = range(n)
+
+    def __getitem__(self, index: int, /) -> int:
+        n = self.indexes[index]
+        return trunc(((PHI**n - PSI**n) / SQRT5).to_integral_exact())
+
+    def __len__(self, /) -> int:
+        return self.size
+
+    def __iter__(self, /) -> Iterator[int]:
+        data = fib_deque()
+        if (n := self.size) > 2:
+            n -= 2
+        else:
+            return it.islice(data, n)
+        return it.chain(
+            data,
+            it.filterfalse(data.append, map(sum, it.repeat(data, n))),
+        )
+
+    def __repr__(self, /):
+        return f"Fib({self.size!r})"
+
+    def __reversed__(self, /):  # PENDING
+        data = deque((-self[-1], self[-2]))
+        if (n := self.size) > 2:
+            n -= 2
+        else:
+            return reversed(data) if n == 2 else it.islice(data, n)
+        return map(
+            abs,
+            it.chain(
+                data,
+                it.filterfalse(data.append, map(sum, it.repeat(data, n))),
+            ),
+        )
+
+    def to_list(self, /) -> list[int]:
+        seq = []
+        seq.extend(it.islice(it.accumulate(it.chain((0, 1), seq)), self.size))
+        return seq
 
 
 def main():
     from sys import argv
 
-    fibs = polars_fib(int(argv[-1]), pl.UInt64)
-    print(fibs.collect())
+    n = int(argv[-1])
+
+    fb = Fib(n)
+    print(fb, fb.to_list() == list(fb))
 
 
 if __name__ == "__main__":
