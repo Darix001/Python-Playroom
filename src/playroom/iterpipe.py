3a0df0e6f5a87262cf5d105cfaf46d4f8cf836ba
Darix001 <100180688+Darix001@users.noreply.github.com>
2026-03-31 13:38:43 -0400

    Refactor iterpipe.py with improved function handling
    new pipeexprs and from expr

--- src/playroom/iterpipe.py
+++ src/playroom/iterpipe.py
@@ -2,21 +2,18 @@ from __future__ import annotations
 
 import builtins
 import dataclasses as dt
+import functools as ft
 import itertools as it
 import operator as op
-import re
 from collections.abc import Callable, Iterable, Iterator
-from functools import partial, reduce
-from types import MethodType, ModuleType
-from typing import Any, Type, TypeVar
+from types import MethodType, ModuleType, SimpleNamespace
+from typing import Any, Self, SupportsIndex, Type, TypeVar
 
 ifuncs = ModuleType(
     "ifuncs",
     doc="This is the first module that is looked up by the register_method "
     "classmethod of Iter class. the register_method is called by __getattr__.",
 )
-
-factories = {}
 
 modules: list[dict[str, Callable]] = [*map(vars, (ifuncs, builtins, it))]
 
@@ -25,88 +22,18 @@ def add_lookup_module(module: ModuleType) -> None:
     modules.append(vars(module))
 
 
-def search_func(function_name: str, /):
-    if func := next(
-        filter(None, map(op.methodcaller("get", function_name), modules)), None
-    ):
-        return func
-    else:
-        raise ValueError("No such function found with name: " + function_name)
-
-
-T_composed = TypeVar("T_composed")
-R = TypeVar("R")
-V = TypeVar("V")
-C = TypeVar("C")
-
-
-class BaseIter(Iterable):
-    __slots__ = ()
-
-    def __getattr__(self, attr: str, /) -> Callable[..., ipartial]:
-        try:
-            return MethodType(self.register_method(attr), self)
-        except ValueError as e:
-            raise AttributeError from e
-
-    def flatten(self, /) -> ipartial:
-        return ipartial(it.chain.from_iterable, self)
-
-    def reduce(self, func: Callable[[Any, Any], V], /, *initial) -> V:
-        return reduce(func, self, *initial) if initial else reduce(func, self)
-
-    def scalar(self, func: Callable[[Iterable], R]) -> R:
-        return func(self)
-
-    @classmethod
-    def register_method(cls: Type[C], method_name: str, /) -> Callable[[...], ipartial]:
-        for key, factory in factories.items():
-            if match := key.match(method_name):
-                method = factory(match)
-                break
-        else:
-            fn = search_func(method_name)
-
-            def method(self, /, *args, **kw) -> ipartial:
-                return ipartial(fn, self, *args, **kw)
-
-        setattr(BaseIter, method_name, method)
-        return method
-
-
-class ipartial(partial, BaseIter):
-    __slots__ = ()
-    __iter__ = partial.__call__
-
-
-@dt.dataclass(slots=True)
-class Iter(BaseIter):
-    iterable: Iterable = ()
-
-    def __iter__(self, /) -> Iterator:
-        return iter(self.iterable)
-
-
-imethod_type = Callable[..., ipartial]
-factory_type = Callable[[re.Match[str]], imethod_type]
-
-
-def imethod_factory(func: factory_type | str, /):
-    return (
-        register_imethod_factory(func, func.__name__)
-        if callable(func)
-        else partial(register_imethod_factory, expr=func)
+def search_func(function_name: str, /, default: Any = None):
+    return next(
+        filter(None, map(op.methodcaller("get", function_name), modules)), default
     )
 
 
-def register_imethod_factory(func: factory_type, expr: str) -> factory_type:
-    factories[re.compile(expr)] = func
-    return func
+T_composed = TypeVar("T_composed")
 
 
-@imethod_factory("composed_?")
-def composed_pipe(match: re.Match[str], /) -> imethod_type:
-    if pipe_func := search_func(match.string[match.end() :]):
+def composed_pipe(method_name: str, /) -> Callable[..., ipartial] | None:
+    left, sep, function_name = method_name.partition("composed_")
+    if not left and sep and (pipe_func := search_func(function_name)):
 
         def method(
             iterable, *args: T_composed, key: Callable[[T_composed], Any] | None = None
@@ -118,17 +45,124 @@ def composed_pipe(match: re.Match[str], /) -> imethod_type:
                 iterable = ipartial(pipe_func, func, iterable)
             return iterable
 
+
+factories = {"composed": composed_pipe}
+
+R = TypeVar("R")
+V = TypeVar("V")
+C = TypeVar("C")
+
+constants: SimpleNamespace[str, int] = SimpleNamespace()
+for i, const_attr in enumerate(("ATTR", "ITEM", "METHOD")):
+    setattr(constants, const_attr, i)
+
+
+@dt.dataclass(slots=True, frozen=True)
+class PipeExpr:
+    pipe: list[
+        tuple[str | Any]
+        | tuple[str, tuple, dict[str, Any]]
+        | tuple[tuple, dict[str, Any]]
+    ] = dt.field(default_factory=[].copy)
+    func_ids: bytearray = dt.field(default_factory=bytearray().copy)
+    funcs = (op.attrgetter, op.itemgetter, op.methodcaller)
+
+    def __getattr__(self, attr: str, /) -> Self:
+        if last_attr := self.last_attr():
+            self.pipe[-1] = (f"{last_attr}.{attr}",)
+        else:
+            self.add(constants.ATTR, (attr,))
+        return self
+
+    def __call__(self, *args, **kw) -> Self:
+        if last_attr := self.last_attr():
+            self.func_ids[-1] = constants.METHOD
+            self.pipe[-1] = (last_attr + ".__call__", args, kw)
+        else:
+            self.add(constants.METHOD, ("__call__", args, kw))
+
+        self.pipe[-1] = (last_attr, args, kw)
+        return self
+
+    def __getitem__(self, /, item) -> Self:
+        self.add(constants.ITEM, (item,))
+        return self
+
+    def add(self, /, func_id: int, obj: tuple):
+        self.pipe.append(obj)
+        self.func_ids.append(func_id)
+
+    def last_attr(self, /):
+        if (funcs := self.func_ids) and not funcs[-1]:
+            return self.pipe[-1][-1]
+
+    def copy(self, /) -> Self | PipeExpr:
+        return type(self)(self.pipe.copy(), self.func_ids.copy())
+
+
+class BaseIter(Iterable):
+    __slots__ = ()
+
+    def __getattr__(self, attr: str, /) -> Callable[..., ipartial]:
+        return MethodType(self.register_method(attr), self)
+
+    def flatten(self, /) -> ipartial:
+        return ipartial(it.chain.from_iterable, self)
+
+    def reduce(self, func: Callable[[Any, Any], V], /, *initial) -> V:
+        return ft.reduce(func, self, *initial) if initial else ft.reduce(func, self)
+
+    def scalar(self, func: Callable[[Iterable], R]) -> R:
+        return func(self)
+
+    def repeat_each(iterable, times: SupportsIndex):
+        return ipartial(map, it.repeat, iterable, it.repeat(times)).flatten()
+
+    @classmethod
+    def register_method(cls: Type[C], method_name: str, /) -> Callable[[...], ipartial]:
+        if not (
+            fn := next(
+                filter(None, map(op.methodcaller("get", method_name), modules)), None
+            )
+        ):
+            raise ValueError("Function Name not found: " + method_name)
+
+        if not (
+            method := next(
+                map(
+                    op.methodcaller("__call__", method_name),
+                    factories.values(),
+                )
+            )
+        ):
+
+            def method(self, /, *args, **kw) -> ipartial:
+                return ipartial(fn, self, *args, **kw)
+
+        setattr(cls, method_name, method)
         return method
 
-    else:
-        raise
+    def with_pipe(iterable, /, pipe: PipeExpr) -> Self | ipartial:
+        for func in map(ft.partial(op.getitem, pipe.funcs), pipe.func_ids):
+            iterable = ipartial(map, func, iterable)
+        return iterable
 
 
-factories: dict[re.Pattern[str], factory_type]
+class ipartial(ft.partial, BaseIter):
+    __slots__ = ()
+    __iter__ = ft.partial.__call__
+
+
+@dt.dataclass(slots=True)
+class Iter(BaseIter):
+    iterable: Iterable = ()
+
+    def __iter__(self, /) -> Iterator:
+        return iter(self.iterable)
 
 
 if __name__ == "__main__":
-    a = Iter[str]("DXctuIvfUTFD^%4#^%*&GOGuibcTRxcKY").composed_filter(
+    a = Iter[str]("DXctuIvfUTFD^%4#^%*&GOGuibcTRxcKY").filter(
         str.isalpha,
         str.islower,
         str.isascii,
