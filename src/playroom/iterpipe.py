9a38963a3fbb120c1faffb71ac8f82ca19a199f7
Dariel Buret <darielburetrivera@gmail.com>
2026-03-31 14:10:23 -0400

    correctly added methodtools, corrected iterpipe errors

--- src/playroom/iterpipe.py
+++ src/playroom/iterpipe.py
@@ -5,6 +5,7 @@ import dataclasses as dt
 import functools as ft
 import itertools as it
 import operator as op
+import re
 from collections.abc import Callable, Iterable, Iterator
 from types import MethodType, ModuleType, SimpleNamespace
 from typing import Any, Self, SupportsIndex, Type, TypeVar
@@ -19,45 +20,19 @@ modules: list[dict[str, Callable]] = [*map(vars, (ifuncs, builtins, it))]
 
 factories = {}
 
+
 def add_lookup_module(module: ModuleType) -> None:
     modules.append(vars(module))
 
 
-def search_func(function_name: str, /, default: Any = None):
-    return next(
-        filter(None, map(op.methodcaller("get", function_name), modules)), default
-    )
-
-
-T_composed = TypeVar("T_composed")
-
-def imethod_factory(func: factory_type | str, /):
-    return (
-        register_imethod_factory(func, func.__name__)
-        if callable(func)
-        else partial(register_imethod_factory, expr=func)
-    )
-
-
-def register_imethod_factory(func: factory_type, expr: str) -> factory_type:
-    factories[re.compile(expr)] = func
+def search_func(function_name: str, /):
+    func = next(filter(None, map(op.methodcaller("get", function_name), modules)), None)
+    if func is None:
+        raise ValueError("Function Name not found: " + function_name)
     return func
 
 
-@imethod_factory("composed_?")
-def composed_pipe(method_name: str, /) -> Callable[..., ipartial] | None:
-    left, sep, function_name = method_name.partition("composed_")
-    if not left and sep and (pipe_func := search_func(function_name)):
-
-        def method(
-            iterable, *args: T_composed, key: Callable[[T_composed], Any] | None = None
-        ) -> ipartial:
-            funcs = reversed(args)
-            if key is not None:
-                funcs = map(key, args)
-            for func in funcs:
-                iterable = ipartial(pipe_func, func, iterable)
-            return iterable
+T_composed = TypeVar("T_composed")
 
 
 R = TypeVar("R")
@@ -132,21 +107,10 @@ class BaseIter(Iterable):
 
     @classmethod
     def register_method(cls: Type[C], method_name: str, /) -> Callable[[...], ipartial]:
-        if not (
-            fn := next(
-                filter(None, map(op.methodcaller("get", method_name), modules)), None
-            )
-        ):
-            raise ValueError("Function Name not found: " + method_name)
-
-        if not (
-            method := next(
-                map(
-                    op.methodcaller("__call__", method_name),
-                    factories.values(),
-                )
-            )
-        ):
+        for regexp, fn in factories.items():
+            if match := regexp.match(method_name):
+                method = fn(match)
+        else:
 
             def method(self, /, *args, **kw) -> ipartial:
                 return ipartial(fn, self, *args, **kw)
@@ -173,10 +137,45 @@ class Iter(BaseIter):
         return iter(self.iterable)
 
 
+factory_type = Callable[[re.Match[str]], Callable[..., ipartial]]
+
+
+def imethod_factory(func, /):
+    return (
+        register_imethod_factory(func, func.__name__)
+        if callable(func)
+        else ft.partial(register_imethod_factory, expr=func)
+    )
+
+
+def register_imethod_factory(func: factory_type, expr: str) -> factory_type:
+    factories[re.compile(expr)] = func
+    return func
+
+
+@imethod_factory("composed_?")
+def composed_pipe(match: re.Match[str], /) -> Callable[..., ipartial]:
+    pipe_func = search_func(match.string[match.end() :])
+
+    def method(
+        iterable, *args: T_composed, key: Callable[[T_composed], Any] | None = None
+    ) -> ipartial:
+        print(89)
+        funcs = reversed(args)
+        if key is not None:
+            funcs = map(key, args)
+        for func in funcs:
+            iterable = ipartial(pipe_func, func, iterable)
+        return iterable
+
+    return method
+
+
 if __name__ == "__main__":
-    a = Iter[str]("DXctuIvfUTFD^%4#^%*&GOGuibcTRxcKY").filter(
+    a = Iter[str]("DXctuIvfUTFD^%4#^%*&GOGuibcTRxcKY").composed_filter(
         str.isalpha,
         str.islower,
         str.isascii,
     )
-    print(a.scalar(",".join), a)
+    print(a)
+    print(a.scalar(",".join))
