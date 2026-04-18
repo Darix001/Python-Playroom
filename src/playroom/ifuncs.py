b775251ae18a0f49d40e840eaace7fecf9bfb7e5
Dariel Buret <darielburetrivera@gmail.com>
2026-04-18 11:46:36 -0400

    Update ifuncs.py

--- src/playroom/ifuncs.py
+++ src/playroom/ifuncs.py
@@ -1,21 +1,12 @@
 from collections.abc import Callable, Iterable, Iterator
 from functools import partial
-from itertools import chain, islice, starmap
+from itertools import chain, islice
 from operator import call
-from typing import Any, TypeVar
-
-T_zip_with = TypeVar("T_zip_with")
-R_zip_with = TypeVar("R_zip_with")
+from typing import Any
 
 
 def map_keywords(funcs: Iterable[Callable], **kw) -> Iterator[Any]:
     return map(partial(call, **kw), funcs)
-
-
-def zip_with(
-    func: Callable[[T_zip_with], R_zip_with], *iterables: Iterable[T_zip_with]
-) -> Iterator[R_zip_with]:
-    return starmap(func, zip(*iterables))
 
 
 def insert(iterable: Iterable, n: int, *objs: Any) -> Iterator:
