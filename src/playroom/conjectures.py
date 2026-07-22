759d2be38292146bc8c4ccd3316c274402d37ce1
Dariel Buret <darielburetrivera@gmail.com>
2026-07-22 09:59:45 -0400

    Update conjectures.py

--- src/playroom/conjectures.py
+++ src/playroom/conjectures.py
@@ -1,18 +1,34 @@
-from collections.abc import Generator
+from collections.abc import Callable, Generator
 from itertools import islice
-from math import trunc
+from operator import methodcaller
 
 
-def collatz[T](x: T) -> Generator[T]:
-    while x != 1:
-        div, mod = divmod(x, 2)
-        yield (x := (x * 3) + 1 if mod else div)
+def conjecture_deco(
+    func: Callable[[int], int],
+) -> Callable[[int], Generator[int]]:
+    def generator(x: int) -> Generator[int]:
+        while x != 1:
+            div, mod = divmod(x, 2)
+            yield (x := func(x * 3) if mod else div)
+
+    return generator
 
 
-def juggler[T](x: T) -> Generator[T]:
-    while x != 1:
-        div, mod = divmod(x, 2)
-        yield (x := trunc((x * 3) / 2) if mod else div)
+collatz = conjecture_deco(methodcaller("__add__", 1))
+
+juggler = conjecture_deco(methodcaller("__floordiv__", 2))
+
+
+# def collatz[T](x: T) -> Generator[T]:
+#     while x != 1:
+#         div, mod = divmod(x, 2)
+#         yield (x := (x * 3) + 1 if mod else div)
+
+
+# def juggler[T](x: T) -> Generator[T]:
+#     while x != 1:
+#         div, mod = divmod(x, 2)
+#         yield (x := trunc((x * 3) / 2) if mod else div)
 
 
 a = collatz(120)
